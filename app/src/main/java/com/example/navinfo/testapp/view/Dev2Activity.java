e389044873bc91821ec3a95e53e8fb640450069c
huguilin <hugl@mapbar.com>
2018-07-20 10:53:44 +0800

    Dev分支修改提交

--- app/src/main/java/com/example/navinfo/testapp/view/Dev2Activity.java
+++ app/src/main/java/com/example/navinfo/testapp/view/Dev2Activity.java
@@ -1,4 +1,13 @@
 package com.example.navinfo.testapp.view;
 
-public class Dev2Activity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Dev2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
