f32594e757ce9b452da075167deb8633c038e3a7
huguilin <hugl@mapbar.com>
2018-07-20 10:59:29 +0800

    Dev修改了Dev2Activity和DevActivity文件

--- app/src/main/java/com/example/navinfo/testapp/view/Dev2Activity.java
+++ app/src/main/java/com/example/navinfo/testapp/view/Dev2Activity.java
@@ -9,5 +9,6 @@ public class Dev2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
--- app/src/main/java/com/example/navinfo/testapp/view/DevActivity.java
+++ app/src/main/java/com/example/navinfo/testapp/view/DevActivity.java
@@ -1,4 +1,7 @@
 package com.example.navinfo.testapp.view;
 
 public class DevActivity {
+
+    private String user;
+    private String phone;
 }
